35b8930834e263d2cb47ab6bdebeb8b8c2f81eed
Simon Chuang <simon.s.chuang@gmail.com>
2017-05-13 01:25:05 +0800

    Use drfdocs as restful web docs

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -1,23 +1,16 @@
-"""portal URL Configuration
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+#from django.contrib.auth.models import User
+#from rest_framework import routers, serializers, viewsets
+#from rest_framework.schemas import get_schema_view
+#from instances.views import InstanceList, InstanceDetail
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^', include('web.urls')),
-    url(r'^', include('api.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include('rest_framework_docs.urls')),
+
+    # API
+    url(r'^instances/', view=include('instances.urls')),
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
